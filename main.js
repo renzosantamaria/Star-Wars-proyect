3d882e9c2a0e17be777f9bc5b77d404aa20b0dfb
renzosantamaria <renzo.santamaria@outlook.com>
2020-11-04 02:19:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let pageNum = 1 // Vi hämtar en character list från den första page som finns
 
 //GRUNDEN TILL ATT FÅ FRAM DATA FRÅN ETT API PÅ ETT ASYNCRONT SÄTT------------------
 async function getStarWarsData(page) {//Gjorde om så att funktionerna tar emot ett page-number variabel som parameter
-    const req = await fetch (`http://swapi.dev/api/people/?page= + ${page}`) // här blir våran fetch dynamiskt beroende på vilken sida man vill fetcha
+    const req = await fetch (`https://swapi.dev/api/people/?page= + ${page}`) // här blir våran fetch dynamiskt beroende på vilken sida man vill fetcha
     const res = await req.json()
     //currentFetch = await req.json()
     return res
